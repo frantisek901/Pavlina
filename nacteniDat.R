7952cf8914d0ed69a408903a4a321c53993b5350
František Kalvas <39860721+frantisek901@users.noreply.github.com>
2021-04-25 07:04:24 +0200

    Finalní skript pro úpravu dat

--- nacteniDat.R
+++ nacteniDat.R
@@ -18,17 +18,23 @@ library(readr)
 library(stringr)
 library(httr)
 library(ggplot2)
+library(forcats)
+
 
 
 # ## Stažení dat: ---------------------------------------------------------
 
 GET('http://207.154.245.100/pgg/monitor/data/*',  ## Adresa naší hry
     write_disk('aktualniData.zip', overwrite = TRUE))
+GET('http://134.122.66.93/pgg/monitor/data/*',  ## Adresa naší hry
+    write_disk('aktualniData2.zip', overwrite = TRUE))
 
 archive = "aktualniData.zip"
 files = unzip(archive, list = T) %>% 
-  filter(Length > 0) 
+  filter(Length > 0) %>% filter(str_detect(Name, ".csv") )%>% 
+  filter(!str_detect(Name, "000015"))  # V místnosti 15 je jen jeden soubor -- vstupní dotazník, jinak nic, jde tedy o nedohranou hru
 files
+
 
 
 # ## Naètení dat ze *.zip archivu: ----------------------------------------
@@ -41,36 +47,51 @@ files
 
 bonus = unz(archive, files$Name[1]) %>% read_csv() %>% rename(player = id, celkem = bonus) 
 emaily = unz(archive, files$Name[2]) %>% read_csv() %>% rename(email = value)
-PGG = unz(archive, files$Name[4]) %>% read_csv()
-Q1 = unz(archive, files$Name[5]) %>% read_csv()
-Q2 = unz(archive, files$Name[6]) %>% read_csv()
+PGG = unz(archive, files$Name[3]) %>% read_csv()
+Q1 = unz(archive, files$Name[4]) %>% read_csv()
+Q2 = unz(archive, files$Name[5]) %>% read_csv()
 
-for (f in 1:(nrow(files)/6 - 1)) {
-  bonus = unz(archive, files$Name[f*6 + 1]) %>% read_csv() %>% 
+for (f in 1:(nrow(files)/5 - 1)) {
+  bonus = unz(archive, files$Name[f*5 + 1]) %>% read_csv() %>% 
     rename(player = id, celkem = bonus) %>% rbind(bonus, .) 
 
-  emaily = unz(archive, files$Name[f*6 + 2]) %>% read_csv() %>% 
+  emaily = unz(archive, files$Name[f*5 + 2]) %>% read_csv() %>% 
     rename(email = value) %>% rbind(emaily, .)
 
-  PGG = unz(archive, files$Name[f*6 + 4]) %>% read_csv() %>% rbind(PGG, .)
+  PGG = unz(archive, files$Name[f*5 + 3]) %>% read_csv() %>% rbind(PGG, .)
 
-  Q1 = unz(archive, files$Name[f*6 + 5]) %>% read_csv() %>% rbind(Q1, .)
+  Q1 = unz(archive, files$Name[f*5 + 4]) %>% read_csv() %>% rbind(Q1, .)
 
-  Q2 = unz(archive, files$Name[f*6 + 6]) %>% read_csv() %>% rbind(Q2, .)
+  Q2 = unz(archive, files$Name[f*5 + 5]) %>% read_csv() %>% rbind(Q2, .)
 }  
+
 
 
 # ## Spojení dat ----------------------------------------------------------
 
+## Kvùli úpravì dat si musím napsat rychle vlastní funkci,
+# je to rekódování 4 promìnných, proto je tam nìkolik kategorií...
+uprava = function(x) {
+  recode(x, `7-velmi dobøe` = "7", `7-velmi blízký` = "7", `7-velmi spolupracuji` ="7")
+}
+
 ## Tohle jsou asi data, se kterými budete pracovat nejvíc --
 # charakteristiky respondentù a jak ovlivòují celkový výsledek
 vysledek = left_join(bonus, emaily) %>% left_join(Q1) %>% left_join(Q2, by = c("player", "session")) %>% 
-  group_by(session) %>% mutate(soucetSkupiny = sum(celkem)) %>% 
-  select(names(vysledek)[1:4], soucetSkupiny, names(vysledek)[5:16])
+  group_by(session) %>% mutate(soucetSkupiny = sum(celkem))  %>% 
+  select(names(.)[1:3], soucetSkupiny, names(.)[4:16], -starts_with("time"), -type) %>% ungroup() %>% 
+  mutate(across(.cols = znajiSe:kooperujeCiz, 
+                .fns = ~(recode(., `7-velmi dobøe` = "7", `7-velmi blízký` = "7", 
+                                  `7-velmi spolupracuji` ="7", `1-vùbec nespolupracuji` = "1",
+                                  `1-vùbec` = "1") %>% as.integer())),  # `stará hodnota` = "nová hodnota"
+         across(.cols = communication:strategy,
+                .fns = as_factor)) %>% 
+  rename(realHrac = real1, realSkup = real2) # `nový název` = `starý název`
+glimpse(vysledek)
 
 ## A takto si mùžeme rovnou nechat vyjet poøadí hráèù!
 vyhodnoceni = vysledek %>% ungroup() %>% arrange(., desc(soucetSkupiny), desc(celkem)) %>% 
-  select(email, soucetSkupiny, celkem)
+  select(email, soucetSkupiny, celkem) #%>% filter(!str_detect(email, "fake@"))
 vyhodnoceni
 
 ## Na pøehled vývoje hry asi bude staèit dfPGG, ale kdyby to bylo potøeba spojit s kontextovými dat,
@@ -80,9 +101,7 @@ celkova
 
 
 
-
 # ## Vývojový graf --------------------------------------------------------
-library(ggplot2)
 ggplot(PGG, aes(x = stage.round, y = contribution, col = as.factor(player))) +
   geom_line()
 
@@ -90,3 +109,6 @@ PGG %>% group_by(stage.round, session) %>% summarise(contribution = mean(contrib
   ggplot(aes(x = stage.round, y = contribution, col = session)) +
   geom_line()
 
+PGG %>% group_by(stage.round) %>% summarise(contribution = mean(contribution)) %>% 
+  ggplot(aes(x = stage.round, y = contribution)) +
+  geom_line()
