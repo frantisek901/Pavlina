164353fa118b58962bf5f82bf39381ee43fa8c18
František Kalvas <39860721+frantisek901@users.noreply.github.com>
2021-05-02 09:09:32 +0200

    aktuální data KA1

--- nacteniDat.R
+++ nacteniDat.R
@@ -24,11 +24,14 @@ library(forcats)
 
 # ## Stažení dat: ---------------------------------------------------------
 
+## Stáhneme data ze serverù
 GET('http://207.154.245.100/pgg/monitor/data/*',  ## Adresa naší hry
     write_disk('aktualniData.zip', overwrite = TRUE))
 GET('http://134.122.66.93/pgg/monitor/data/*',  ## Adresa naší hry
     write_disk('aktualniData2.zip', overwrite = TRUE))
 
+
+## Získáme seznam souborù ve stažených datech
 archive = "aktualniData.zip"
 files = unzip(archive, list = T) %>% 
   filter(Length > 0) %>% filter(str_detect(Name, ".csv") )%>% 
@@ -37,6 +40,11 @@ files = unzip(archive, list = T) %>%
 files
 
 
+## Na pøipsání èasu, kdy hra probìhla si vytvoøíme zvláštní soubor
+datumy = files[seq(5, nrow(files),5),] %>% 
+  mutate(Name = substr(Name, 6, 15)) %>% select(-Length) %>% 
+  rename(session = Name, date = Date)
+datumy
 
 # ## Naètení dat ze *.zip archivu: ----------------------------------------
 
@@ -73,8 +81,9 @@ for (f in 1:(nrow(files)/5 - 1)) {
 ## Tohle jsou asi data, se kterými budete pracovat nejvíc --
 # charakteristiky respondentù a jak ovlivòují celkový výsledek
 vysledek = left_join(bonus, emaily) %>% left_join(Q1) %>% left_join(Q2, by = c("player", "session")) %>% 
+  left_join(datumy, by = c("session")) %>% 
   group_by(session) %>% mutate(soucetSkupiny = sum(celkem))  %>% 
-  select(names(.)[1:3], soucetSkupiny, names(.)[4:16], -starts_with("time"), -type) %>% ungroup() %>% 
+  select(names(.)[1:3], soucetSkupiny, names(.)[4],date, names(.)[5:16], -starts_with("time"), -type) %>% ungroup() %>% 
   mutate(across(.cols = znajiSe:kooperujeCiz, 
                 .fns = ~(recode(., `7-velmi dobøe` = "7", `7-velmi blízký` = "7", 
                                   `7-velmi spolupracuji` ="7", `1-vùbec nespolupracuji` = "1",
@@ -137,7 +146,7 @@ ggplot(PGG, aes(x = stage.round, y = contribution, group = session)) +
              size = 0.5) +  # Prùmìr za celý turnaj
   facet_wrap(vars(session), nrow = 3) +  # Rozdìlení do panelù podle skupin
   guides(color = F) +
-  labs(caption = "Vysvìtlení pro Pavlínu:
+  labs(caption = "Vysvìtlivky:
   Tenká modrá èára ukazuje prùmìrnou investici do spoleèného úètu za kolo za celý turnaj.
   Tlustá šedá èára ukazuje prùmìrnou investici za kolo v pøíslušné skupinì.
   Modré body ukazují investice jednotlivých hráèù, které byly celé èíslo v intervalu 0--20. 
@@ -157,7 +166,7 @@ ggplot(PGG, aes(x = stage.round, y = contribution, group = stage.round)) +
             size = 1.5) +  # Prùmìr za celý turnaj
   geom_boxplot(alpha = 0.1, fill = "steelblue", col = "steelblue") +
   guides(color = F) +
-  labs(caption = "Vysvìtlení pro Pavlínu:
+  labs(caption = "Vysvìtlivky:
   Tlustá modrá èára ukazuje prùmìrnou investici do spoleèného úètu za kolo za celý turnaj.
   Modré box-ploty ukazují celkovou distribuci investic v jednotlivých kolech.
   Modré body ukazují investice jednotlivých hráèù, které byly celé èíslo v intervalu 0--20. 
@@ -167,3 +176,35 @@ ggplot(PGG, aes(x = stage.round, y = contribution, group = stage.round)) +
   scale_x_continuous(breaks = seq(0, 10, 2)) +
   scale_y_continuous(breaks = seq(0, 20, 2))
 
+
+
+# ## Odmìny ---------------------------------------------------------------
+
+## Nejdøív je potøeba ynovu vygenerovat vyhodnoceni:
+dfv = vysledek %>% ungroup() %>% arrange(., desc(soucetSkupiny), desc(celkem)) %>% 
+  select(email, session, soucetSkupiny, celkem) #%>% 
+  # group_by(email) %>% mutate(x = 1) %>% mutate(x = sum(x)) %>%   # Kontrola, jestli tam není nìjaký email dvakrát.
+  # arrange(., desc(x), email)  # OK, dopadlo to dobøe, každý mail tu jejen jednou!
+# vyhodnoceni
+
+ 
+## Teï je potøeba vùbec nadefinovat vektor s odmìnami a doplnit 0 na délku `vyhodnoceni`:
+odmeny = c(seq(1000, 500, -100), 450, 400, 400, 350, rep(300,3), 250, rep(200, 5), 150,
+           150, rep(100, 6), rep(50, 16), 25, 25, rep(0, nrow(vyhodnoceni) - 45))
+# length((odmeny))
+# sum(odmeny)
+# SUPER! Sedí to!
+
+
+## Tak... Teï pøipojím `odmeny` jako další promìnnou do `vyhodnoceni` a 
+# rovnou každému vypoètu jeho odmìnu:
+# 1) seskupím je podle `soucetSkupiny` a podle `celkem`, tím mi pøirozenì vzniknou skupinky, 
+#    které si budou dìlit odmìny prùmìrem (pøi rovnosti bodù skupiny vytváøí `super-skupiny` 
+#    a uvnitø nich si dìlí odmìnu rovným dílem hráèi se stejným poètem bodù)
+# 2) uvnitø skupin daných rovnými body (jak skupin tak hráèù) spoèítám prùmìrné odmìny
+# 3) když budou body unikátní, zkrátka se replikuje hodnota, když ne, rozdìlí se
+# 4) využívám toho, že žebøíèek hráèù a jejich výsledkù už je seøazený sestupnì, od nejlepší skupiny 
+#    po nejhorší, a totéž s hráèi, když k tomu pak pøidám sestupnì seøazené odmìny, snadno to spoètu
+vyhodnoceni = cbind(dfv, 'odmena' = odmeny) %>% group_by(soucetSkupiny, celkem) %>% 
+  mutate(odmena = mean(odmena))
+vyhodnoceni
